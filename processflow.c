610362e5f8053f9e4f8417ee2d6e0fc56eb515a3
Guilherme Lindemberg <g35844579@gmail.com>
2026-08-22 17:21:24 -0300

    adiconei o cadastro de tarefas. o cadastro só é interrompido quando o usuário escreve exit

--- processflow.c
+++ processflow.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 #include "processflow.h"
 
 int main(){
     char comando[100];
+
+    tarefa t;
 
     while (1)
     {
@@ -17,6 +20,54 @@ int main(){
         if (strcmp(comando, "exit") == 0){
             break;
         }
+
+        if (strncmp(comando, "task ", 5) == 0){
+            char *parte;
+
+            parte = strtok(comando, " ");
+
+            parte = strtok(NULL, " ");
+
+            if (parte == NULL){
+                printf("tarefa não foi encontrada\n");
+                continue;
+            }
+
+            strcpy(t.nome, parte);
+
+            parte = strtok(NULL, " ");
+
+            if (parte == NULL){
+                printf("programa nao foi encontrado\n");
+                continue;
+            }
+
+            strcpy(t.programa, parte);
+
+            t.quantidade_argumentos = 0;
+
+            while ((parte = strtok(NULL, " ")) != NULL){
+                if (t.quantidade_argumentos >= 10){
+                    printf("limite de argumentos atingido\n");
+                    break;
+                }
+
+                strcpy(t.argumentos[t.quantidade_argumentos],parte);
+                t.quantidade_argumentos++;
+            }
+
+            printf("tarefa cadastrada\n");
+            printf("nome: %s\n", t.nome);
+            printf("programa: %s\n", t.programa);
+
+            printf("argumentos: ");
+            for (int i = 0; i < t.quantidade_argumentos; i++){
+                printf(" %s", t.argumentos[i]);
+            }
+
+            printf("\n");
+        }
     }
     return 0;
 }
+
--- processflow.h
+++ processflow.h
@@ -1,3 +1,11 @@
 #ifndef PROCESSFLOW_H
 #define PROCESSFLOW_H
+
+typedef struct tarefa{
+    char nome[100];
+    char programa[100];
+    char argumentos[10][100];
+    int quantidade_argumentos;
+}tarefa;
+
 #endif
